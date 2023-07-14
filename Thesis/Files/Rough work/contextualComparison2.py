10e462446e7ec1024b6876d3d26498f176966d20
Aniruddha Mondal (Student) <amondal8@sundevils.asu.edu>
2023-07-13 23:58:04 -0700

    creating rough work folder

--- Thesis/Files/Rough work/contextualComparison2.py
+++ Thesis/Files/Rough work/contextualComparison2.py
@@ -2,7 +2,7 @@ import openpyxl as op
 import spacy
 
 model1 = "en_core_web_lg"
-filepath= "Thesis/Files/Excel Implementation/Mapping.xlsx"
+filepath= "../Excel Implementation/Mapping.xlsx"
 try:
     nlp = spacy.load(model1)
 except OSError:
@@ -124,16 +124,16 @@ for row2 in worksheet2.iter_rows(min_row=2, max_row=maxr2, min_col=2, max_col=2)
                 if similarity_score > similarity_threshold:
                     # print(f"score: {similarity_score} ")      ********
                           # f"r1:{r1} and r2:{r2}")
-                    userStory_PointR2 = int(worksheet2.cell(row=rwCount2, column=3).value)
-                    if worksheet1.cell(row=rwCount1, column=3).value is None:
-                        userStory_PointR1 = 0
+                    userStory_PointR1 = int(worksheet1.cell(row=rwCount1, column=3).value)
+                    if worksheet2.cell(row=rwCount2, column=3).value is None:
+                        userStory_PointR2 = 0
                     else:
-                        usp = worksheet1.cell(row=rwCount1, column=3).value
-                        userStory_PointR1 = int(usp)
+                        usp = worksheet2.cell(row=rwCount2, column=3).value
+                        userStory_PointR2 = int(usp)
 
-                    userStory_PointR1 = userStory_PointR1+userStory_PointR2
-                    cell = worksheet1.cell(row=rwCount1, column=3)
-                    writeto_excel(cell, userStory_PointR1)
+                    userStory_PointR2 = userStory_PointR1+userStory_PointR2
+                    cell = worksheet2.cell(row=rwCount2, column=3)
+                    writeto_excel(cell, userStory_PointR2)
 
                     mapVal2 = worksheet2.cell(row=rwCount2, column=1).value
                     cell = worksheetMap.cell(row=rwCount2, column=1)
